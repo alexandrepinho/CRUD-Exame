98f26fc60bd81219adbe989616aa24779956dbe9
Alexandre Pinho <alexandrep@cobra001023.saovicente.local>
2018-12-10 13:56:46 -0200

    FIX BUG REFACTORING - Correção no projeto Exame-Cliente
    -Mudança das classes dos pacotes para correção;
    -Remoção de arquivos que não estavam sendo utilizados;

--- Exame-Cliente/src/br/com/exame/client/ExameClient.java
+++ Exame-Cliente/src/br/com/exame/client/ExameClient.java
@@ -15,10 +15,16 @@ public class ExameClient {
 		Service ws = Service.create(url, qname);
 		QName qport = new QName("http://model.crud.com.br/", "ExameServerImplPort");
 
-		ExameServer exam = ws.getPort(qport,ExameServer.class);
+		ExameServer exam = ws.getPort(qport, ExameServer.class);
 
-		Exame exame = exam.busca(1);
+		Exame exame = exam.busca(1); // BUSCA EXEMPLO PELO ID/CODIGO 2 DO EXAME SE EXISTIR
 		System.out.println("Nome do paciente: " + exame.getNomePaciente());
+		System.out.println("Documento do paciente: " + exame.getDocumentoPaciente());
+		System.out.println("Data do exame: " + exame.getDataExame());
+		System.out.println("Data do vencimento: " + exame.getDataVencimento());
+		System.out.println("Finalidade: " + exame.getFinalidade());
+		System.out.println("Medico: " + exame.getMedico().getNome());
+		System.out.println("Tipo do exame: " + exame.getTipoExame().getNome());
 
 	}
 }
